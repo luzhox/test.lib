d5494f1d698adc1d180dd9ea4e7a563b407d2272
Luis Morales <luis.moralesponce@gmail.com>
2022-10-09 15:31:51 -0500

    update

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 interface IButtonProps {
   text?: string
+  color?: string
 }
-const Button = ({ text = '' }: IButtonProps) => {
-  return <div className='button'>{text}</div>
+const Button = ({ text = '', color = '' }: IButtonProps) => {
+  return <div className={`button ${color}`}>{text}</div>
 }
 
 export default Button

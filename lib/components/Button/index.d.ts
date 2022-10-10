7b8ac15d5fe50e87457564354c5650cd5048012c
Luis Morales <luis.moralesponce@gmail.com>
2022-10-09 21:39:08 -0500

    update style

--- lib/components/Button/index.d.ts
+++ lib/components/Button/index.d.ts
@@ -1,7 +1,12 @@
 /// <reference types="react" />
 interface IButtonProps {
     text?: string;
-    color?: string;
+    bgColorHover: string;
+    colorText?: string;
+    colorHover?: string;
+    font?: string;
+    padding?: string;
+    bgColor?: string;
 }
-declare const Button: ({ text, color }: IButtonProps) => JSX.Element;
+declare const Button: ({ text, padding, bgColor, bgColorHover, colorText, colorHover, font, }: IButtonProps) => JSX.Element;
 export default Button;

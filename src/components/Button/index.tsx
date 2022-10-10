170e190b1df5a163c50e3169129a6c385065da95
Luis Morales <luis.moralesponce@gmail.com>
2022-10-09 21:27:50 -0500

    add sample style in button

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import { ButtonUi } from './style/index.js'
 interface IButtonProps {
   text?: string
   color?: string
 }
 const Button = ({ text = '', color = '' }: IButtonProps) => {
-  return <div className={`button ${color}`}>{text}</div>
+  return <ButtonUi className={`button ${color}`}>{text}</ButtonUi>
 }
 
 export default Button
---
+++ src/components/Button/style/index.js
@@ -0,0 +1,14 @@
+import styled from 'style-components'
+
+/* tslint:disable */
+export const ButtonUi = styled.button`
+  background-color: ${props.bgColor || 'white'};
+  padding: ${props.padding || '0px'};
+  color: ${props.color || 'black'};
+  font-family: ${props.font || 'sans-serif'};
+  transition: ease-in-out all 0.3s;
+  &:hover {
+    background-color: ${bgColorHover || 'white'};
+    color: ${props.colorHover || 'black'};
+  }
+`

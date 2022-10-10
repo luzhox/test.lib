6820af5ec8dfb2c980af02f600e305bacfd4499a
Luis Morales <luis.moralesponce@gmail.com>
2022-10-09 21:36:05 -0500

    update styles

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -2,10 +2,34 @@ import React from 'react'
 import { ButtonUi } from './style/index.js'
 interface IButtonProps {
   text?: string
-  color?: string
+  bgColorHover: string
+  colorText?: string
+  colorHover?: string
+  font?: string
+  padding?: string
+  bgColor?: string
 }
-const Button = ({ text = '', color = '' }: IButtonProps) => {
-  return <ButtonUi className={`button ${color}`}>{text}</ButtonUi>
+const Button = ({
+  text = '',
+  padding = '',
+  bgColor = '',
+  bgColorHover = '',
+  colorText = '',
+  colorHover = '',
+  font = '',
+}: IButtonProps) => {
+  return (
+    <ButtonUi
+      bgColorHover={bgColorHover}
+      colorText={colorText}
+      colorHover={colorHover}
+      font={font}
+      padding={padding}
+      bgColor={bgColor}
+    >
+      {text}
+    </ButtonUi>
+  )
 }
 
 export default Button
--- src/components/Button/style/index.js
+++ src/components/Button/style/index.js
@@ -4,7 +4,7 @@ import styled from 'style-components'
 export const ButtonUi = styled.button`
   background-color: ${props.bgColor || 'white'};
   padding: ${props.padding || '0px'};
-  color: ${props.color || 'black'};
+  color: ${props.colorText || 'black'};
   font-family: ${props.font || 'sans-serif'};
   transition: ease-in-out all 0.3s;
   &:hover {
